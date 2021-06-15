44697c93873e9c45536bc8553f9a6815f3d34a56
Patricio Henriquez <49575726+patriciohenriqueze@users.noreply.github.com>
2021-06-14 20:41:13 -0400

    Update unsigned_to_bcd.sv

--- double-dabble-32bits/unsigned_to_bcd.sv
+++ double-dabble-32bits/unsigned_to_bcd.sv
@@ -43,7 +43,7 @@ module unsigned_to_bcd
 	 
 	localparam COUNTER_MAX = 32;
 	
-	(* fsm_encoding = "one_hot" *) enum logic [2:0] {S_IDLE, S_SHIFT, S_ADD3} state, next_state;
+	(* fsm_encoding = "one_hot" *) enum logic [2:0] {S_IDLE, S_SHIFT, S_ADD3} state, state_next;
 
 	logic [31:0] shift, shift_next;
 	logic [31:0] bcd_next;
@@ -123,10 +123,11 @@ module unsigned_to_bcd
 		endcase
 	end
 
-	always @(posedge clk) begin
-		if(reset)
-			{state, shift, bcd, counter} <= 'd0;
-		else
+	always_ff @(posedge clk) begin
+		if(reset) begin
+			state <= S_IDLE;
+			{shift, bcd, counter} <= 'd0;
+		end else
 			{state, shift, bcd, counter} <= {state_next, shift_next, bcd_next, counter_next};
 	end
 
